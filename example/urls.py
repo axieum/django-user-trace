f8cf404c655627925385e07ed80be2c2662509b1
Jonathan Hiles <jonathan@hil.es>
2023-07-02 21:30:48 +1000

    test: use `async` views for asgi tests

--- example/urls.py
+++ example/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
+    path("asgi/", views.aindex_view, name="aindex"),
     path("task/", views.task_add_view, name="task"),
     path("auth/", include("django.contrib.auth.urls")),
 ]
